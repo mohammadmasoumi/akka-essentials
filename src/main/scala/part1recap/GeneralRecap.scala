4c8d545366005408413156f870e86289b5dd1a57
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-08 23:24:39 +0330

    add soem basics

---
+++ src/main/scala/part1recap/GeneralRecap.scala
@@ -0,0 +1,12 @@
+package part1recap
+
+object GeneralRecap extends App {
+
+  val aCondition: Boolean = false // no reassignment
+  var  aVariable = 32
+  aVariable += 1 // aVariable = 43
+
+
+
+
+}
