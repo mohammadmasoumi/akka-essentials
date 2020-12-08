ba756f05a730b6260a7a16b849e48131888872fc
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-08 23:26:43 +0330

    add CodeBlocks

--- src/main/scala/part1recap/GeneralRecap.scala
+++ src/main/scala/part1recap/GeneralRecap.scala
@@ -6,6 +6,15 @@ object GeneralRecap extends App {
   var  aVariable = 32
   aVariable += 1 // aVariable = 43
 
+  // expressions
+  val aConditionedVal = if (aCondition) 42 else 65
+
+  // code blocks
+  val aCodeBlock = {
+    if (aCondition) 74
+    54 // the value of code block is equal to the last expression. here is 54
+  }
+
 
 
 
