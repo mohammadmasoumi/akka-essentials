c4dc6b767d550efb02b2f554d4b0f5ff53df827f
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-08 23:31:02 +0330

    add function and recursion

--- src/main/scala/part1recap/GeneralRecap.scala
+++ src/main/scala/part1recap/GeneralRecap.scala
@@ -1,9 +1,11 @@
 package part1recap
 
+import scala.annotation.tailrec
+
 object GeneralRecap extends App {
 
   val aCondition: Boolean = false // no reassignment
-  var  aVariable = 32
+  var aVariable = 32
   aVariable += 1 // aVariable = 43
 
   // expressions
@@ -15,7 +17,19 @@ object GeneralRecap extends App {
     54 // the value of code block is equal to the last expression. here is 54
   }
 
+  // types
+  // Unit
+  val aUnit: Unit = println("Hello, scala") // side affect
 
+  // functions
+  def aFunction(x: Int): Int = x + 1
 
+  // recursion - TAIL recursion
+  @tailrec
+  def factorial(num: Int, acc: Int = 1): Int = {
+    if (num <= 1) acc
+    else factorial(num - 1, num * acc)
+  }
+  println(factorial((5)))
 
 }
