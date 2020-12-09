adcf1c61aafc84c00625015d0bf77cf202da9a02
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-09 23:55:09 +0330

    add syntactic sugar thread style

--- src/main/scala/part1recap/MultiThreadingRecap.scala
+++ src/main/scala/part1recap/MultiThreadingRecap.scala
@@ -5,5 +5,9 @@ object MultiThreadingRecap extends App {
   val aThread = new Thread(new Runnable {
     override def run(): Unit = println("I'm running in parallel!")
   })
+  // syntactic sugar - more practical
+  val aSyntacticSugarThread = new Thread(() => println("I'm running in parallel!"))
+
   aThread.start()
+  aSyntacticSugarThread.start()
 }
