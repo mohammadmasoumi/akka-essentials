d1e44cfde97d3d70c42a17eed72a4531af8f650e
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-09 23:08:16 +0330

    add partial functions

--- src/main/scala/part1recap/AdvancedRecap.scala
+++ src/main/scala/part1recap/AdvancedRecap.scala
@@ -12,6 +12,20 @@ object AdvancedRecap extends App {
   // validate domain for this partialFunction (42, 43, 44)
   // anything else will throw an exception
 
+  val pf = (x: Int) => x match {
+    case 1 => 42
+    case 2 => 43
+    case 3 => 44
+  } // partial functions are powered by pattern matching
+
+  val aFunction: (Int => Int) = partialFunction
+  try {
+    println(aFunction(1))
+    println(aFunction(42)) // out of partial function domain
+  } catch {
+    case e: MatchError => println(s"Caught a MatchError: $e")
+  }
+
 
 
 }
