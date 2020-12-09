96c42180b369f78bb80d3a17d59e4fdbf17854ce
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-09 23:02:48 +0330

    add pertial function section

--- src/main/scala/part1recap/AdvancedRecap.scala
+++ src/main/scala/part1recap/AdvancedRecap.scala
@@ -3,4 +3,15 @@ package part1recap
 object AdvancedRecap extends App {
 
 
+  // partial function
+  val partialFunction: PartialFunction[Int, Int] = {
+    case 1 => 42
+    case 2 => 43
+    case 3 => 44
+  }
+  // validate domain for this partialFunction (42, 43, 44)
+  // anything else will throw an exception
+
+
+
 }
