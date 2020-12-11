8ee4d0a0968a5f97d03dd792749a582353a200e7
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-11 20:07:50 +0330

    add actor system features

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -5,9 +5,17 @@ import akka.actor.ActorSystem
 object ActorsIntro extends App {
 
   // part1 - actor systems
+  /*
+    - heavy data structure
+    - once per app
+    - handle multi-threading
+    - naming restriction (no space, no underscore, no hyphen, just normal characters)
+   */
   val actorSystem = ActorSystem("FirstActorSystem")
 
   println(actorSystem.name)
+
+
 
 
 }
