6c780589837f5c8f8b1255b848aab7f8fcfc4d4f
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-12 21:40:06 +0330

    add systemActor and system

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -1,14 +1,19 @@
 package part2actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorSystem, Props}
 
 object ActorCapabilities extends App {
 
   class SimpleActor extends Actor {
     override def receive: Receive = {
-      case message: String => println(s"[simple actor] I have received $message")
-
+      case message: String => println(s"[simple actor] I have received `$message`")
     }
   }
+
+  val system = ActorSystem("actorCapabilitiesDemo")
+  val simpleActor = system..actorOf(Props[SimpleActor], "simpleActor")
+
+  simpleActor ! "Hello, actor"
+
 
 }
