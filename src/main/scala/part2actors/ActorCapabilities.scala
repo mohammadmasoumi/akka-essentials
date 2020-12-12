f359b5067cbd76b702416af9443aa48946d2a4e2
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-12 21:48:30 +0330

    update SpecialMessage

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -8,7 +8,7 @@ object ActorCapabilities extends App {
     override def receive: Receive = {
       case message: String => println(s"[simple actor] I have received `$message`")
       case number: Int => println(s"[simple actor] I have received a NUMBER: `$number`")
-      case specialMsg: SpecialMessage => println(s"[simple actor] I have received a SpecialMessage: `$specialMsg`")
+      case SpecialMessage(contents) => println(s"[simple actor] I have received sth SPECIAL: `$contents`")
     }
   }
 
@@ -20,11 +20,10 @@ object ActorCapabilities extends App {
   // 1. messages can be of any type
   simpleActor ! 42
 
+  // any type
   case class SpecialMessage(contents: String)
+
   simpleActor ! SpecialMessage("some special contents!")
-
-
-
 
 
 }
