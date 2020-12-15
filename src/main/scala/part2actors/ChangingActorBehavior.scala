1b8e91704f94de0c2319c151b8093d418db556aa
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-15 23:31:26 +0330

    add mom response to kid acceptance or rejection

--- src/main/scala/part2actors/ChangingActorBehavior.scala
+++ src/main/scala/part2actors/ChangingActorBehavior.scala
@@ -8,8 +8,11 @@ object ChangingActorBehavior extends App {
   object FussyKid {
     val HAPPY = "happy"
     val SAD = "sad"
+
     case object KidAccept
+
     case object KidReject
+
   }
 
   class FussyKid extends Actor {
@@ -35,11 +38,15 @@ object ChangingActorBehavior extends App {
     val CHOCOLATE = "chocolate"
 
     case class MomStart(kidReference: ActorRef)
+
     case class Food(food: String)
+
     case class Ask(message: String) // questions like: do you want to play?
   }
 
   class Mom extends Actor {
+
+    import FussyKid._
     import Mom._
 
     override def receive: Receive = {
@@ -47,6 +54,8 @@ object ChangingActorBehavior extends App {
         // test our interaction
         kid ! Food(VEGETABLE)
         kid ! Ask("do you want to play?")
+      case KidAccept => println("Yeah, my kid is happy!")
+      case KidReject => println("My kid is sad. but as he's healthy")
     }
   }
 
