2ecc1614893f36db250c35420189ddf2961fa687
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-15 23:22:40 +0330

    reply to the message

--- src/main/scala/part2actors/ChangingActorBehavior.scala
+++ src/main/scala/part2actors/ChangingActorBehavior.scala
@@ -1,34 +1,42 @@
 package part2actors
 
 import akka.actor.Actor
-import part2actors.ChangingActorBehavior.FussyKid.HAPPY
 
 object ChangingActorBehavior extends App {
 
   // object companion class
   object FussyKid {
+
     case object KidAccept
+
     case object KidReject
+
     val HAPPY = "happy"
     val SAD = "sad"
   }
 
   class FussyKid extends Actor {
+
     import FussyKid._
     import Mom._
 
     // internal state of the kid
     var state = HAPPY
+
     override def receive: Receive = {
       case Food(VEGETABLE) => state = SAD
       case Food(CHOCOLATE) => state = HAPPY
-      case Ask(message: String) =>
+      case Ask(_) =>
+        if (state == HAPPY) sender() ! KidAccept
+        else sender() ! KidReject
     }
   }
 
   // object companion class
   object Mom {
+
     case class Food(food: String)
+
     case class Ask(message: String) // questions like: do you want to play?
     val VEGETABLE = "veggies"
     val CHOCOLATE = "chocolate"
