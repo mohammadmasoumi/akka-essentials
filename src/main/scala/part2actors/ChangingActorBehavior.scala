c4cbd02fc32c5738101b54627765e1c45d26de77
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-15 23:03:37 +0330

    add food class

--- src/main/scala/part2actors/ChangingActorBehavior.scala
+++ src/main/scala/part2actors/ChangingActorBehavior.scala
@@ -4,8 +4,16 @@ import akka.actor.Actor
 
 object ChangingActorBehavior extends App {
 
+
   class FussyKid extends Actor {
     override def receive: Receive = ???
+  }
+
+  // object companion class
+  object Mom {
+
+    case class Food(food: String)
+
   }
 
   class Mom extends Actor {
