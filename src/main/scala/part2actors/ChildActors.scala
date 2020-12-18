19f5142bf29513eac38ffc995046a9f6df75efba
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-18 18:04:02 +0330

    update childActor

--- src/main/scala/part2actors/ChildActors.scala
+++ src/main/scala/part2actors/ChildActors.scala
@@ -5,7 +5,6 @@ import akka.actor.Actor
 object ChildActors extends App {
 
   // Actors can create other actors
-
   object Parent {
     case class CreateChild(name: String)
     case class TellChild(name: String)
@@ -16,5 +15,10 @@ object ChildActors extends App {
     override def receive: Receive = ???
   }
 
+  class Child extends Actor {
+    override def receive: Receive = {
+      case message: String => println(s"${self.path} I got: $message")
+    }
+  }
 
 }
