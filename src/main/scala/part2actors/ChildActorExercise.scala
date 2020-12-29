34e2c1399b677cf5ef32ec5ae1b6acc8d8440c46
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-30 00:05:42 +0330

    add props

--- src/main/scala/part2actors/ChildActorExercise.scala
+++ src/main/scala/part2actors/ChildActorExercise.scala
@@ -1,25 +1,39 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{Actor, ActorSystem, Props}
 
 object ChildActorExercise extends App {
 
   // distributed word counting
   object WordCounterMaster {
+    def props(name: String): Props = Props(new WordCounterMaster(name))
+
     case class Initialize(nChildren: Int)
+
     case class WordCountTask(/* TODO */ text: String)
+
     case class WordCountReply /* TODO */ (count: Int)
+
   }
 
-  class WordCounterMaster extends Actor {
+  class WordCounterMaster(name: String) extends Actor {
     override def receive: Receive = ???
+  }
+
+  object WordCounterWorker {
+    def props(name: String): Props = Props(new WordCounterWorker(name))
   }
 
   class WordCounterWorker extends Actor {
     override def receive: Receive = ???
   }
 
+  import WordCounterMaster._
+
   val system = ActorSystem("WordCounterSystem")
+  val wordCounterMaster = system.actorOf(WordCounterMaster.props("master"))
+
+  wordCounterMaster ! Initialize(10)
 
   /*
     create WordCounterMaster
