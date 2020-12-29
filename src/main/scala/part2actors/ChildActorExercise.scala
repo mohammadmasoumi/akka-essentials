59054f86638e8cb270d45bc044e9eb764eb2f1da
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-29 23:34:07 +0330

    add wordcounter

--- src/main/scala/part2actors/ChildActorExercise.scala
+++ src/main/scala/part2actors/ChildActorExercise.scala
@@ -1,7 +1,13 @@
 package part2actors
 
+import akka.actor.Actor
+
 object ChildActorExercise extends App {
 
   // distributed word counting
+
+  class WordCounterMaster extends Actor {
+    override def receive: Receive = ???
+  }
 
 }
