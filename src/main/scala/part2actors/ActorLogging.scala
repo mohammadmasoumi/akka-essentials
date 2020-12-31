4d5662da3d77eb1407680697b2c03bd493e218f5
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-12-31 23:24:37 +0330

    add simple logger

--- src/main/scala/part2actors/ActorLogging.scala
+++ src/main/scala/part2actors/ActorLogging.scala
@@ -1,5 +1,11 @@
 package part2actors
 
+import akka.actor.Actor
+
 object ActorLogging extends App {
+
+  class SimpleActorWithExplicitLogger extends Actor {
+    override def receive: Receive = ???
+  }
 
 }
