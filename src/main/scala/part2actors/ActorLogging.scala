82771eadd7ba9681974fdf095602a52ac3d33aae
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-02 08:21:34 +0330

    add logger

--- src/main/scala/part2actors/ActorLogging.scala
+++ src/main/scala/part2actors/ActorLogging.scala
@@ -1,12 +1,22 @@
 package part2actors
 
 import akka.actor.Actor
+import akka.event.Logging
 
 object ActorLogging extends App {
-
   class SimpleActorWithExplicitLogger extends Actor {
+    val logger = Logging(context.system, this)
+
     override def receive: Receive = {
       case message => // log it?
+        /*
+         4 levels of logging:
+          1. DEBUG
+          2. INFO
+          3. WARNING/WARN
+          4. ERROR
+         */
+        logger.info(s"message: $message")
     }
   }
 
