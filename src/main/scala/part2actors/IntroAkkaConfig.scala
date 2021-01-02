8fdf7f91313c81e7497e6483e9408bc1f57f63b3
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-02 23:11:41 +0330

    add inline configuration

--- src/main/scala/part2actors/IntroAkkaConfig.scala
+++ src/main/scala/part2actors/IntroAkkaConfig.scala
@@ -2,4 +2,16 @@ package part2actors
 
 object IntroAkkaConfig extends App {
 
+  /**
+   * 1 - inline configuration
+   *
+   */
+
+  val configString =
+    """
+      | akka {
+      |   loglevel = "DEBUG"
+      | }
+      |""".stripMargin
+
 }
