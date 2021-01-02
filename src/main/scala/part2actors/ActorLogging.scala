3908aa0b39c2697453b2a199b56edb88c107fab2
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-02 08:29:04 +0330

    add ActorLogging treat

--- src/main/scala/part2actors/ActorLogging.scala
+++ src/main/scala/part2actors/ActorLogging.scala
@@ -1,9 +1,11 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.event.Logging
 
 object ActorLogging extends App {
+
+  // #1 - explicit logging
   class SimpleActorWithExplicitLogger extends Actor {
     val logger = Logging(context.system, this)
 
@@ -19,8 +21,19 @@ object ActorLogging extends App {
         logger.info(s"message: $message")
     }
   }
+
   val system = ActorSystem("LoggingDemo")
-  val actor = system.actorOf(Props[SimpleActorWithExplicitLogger], "loggingActor")
-  actor ! "Logging a simple message"
+  val actor1 = system.actorOf(Props[SimpleActorWithExplicitLogger], "loggingActor")
+  actor1 ! "Logging a simple message"
+
+  // #2 - ActorLogging
+  class ActorWithLogging extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message: String => log.info(s"message: $message")
+    }
+  }
+  val actor2 = system.actorOf(Props[ActorWithLogging], "actorWithLogging")
+  actor2 ! "Logging a simple message"
+
 
 }
