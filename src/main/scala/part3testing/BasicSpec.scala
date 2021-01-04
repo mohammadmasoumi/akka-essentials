e1d7e355e33c76b4da4047a492cb6dff033c6026
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-04 23:34:32 +0330

    add testkit

--- src/main/scala/part3testing/BasicSpec.scala
+++ src/main/scala/part3testing/BasicSpec.scala
@@ -1,9 +1,14 @@
 package part3testing
 
 import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 // BEST PATTERN: end with `Spec` key
-class BasicSpec extends TestKit(ActorSystem("BasicSpec")) {
+class BasicSpec extends TestKit(ActorSystem("BasicSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
+
 
 }
