e90689b46ffa24954e4984b30e446420549ab9b3
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-07 18:59:33 +0330

    add CreditCard

---
+++ src/main/scala/part3testing/InterceptingLogSpec.scala
@@ -0,0 +1,43 @@
+package part3testing
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+class InterceptingLogSpec extends TestKit(ActorSystem("InterceptingLogSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+}
+
+object InterceptingLogSpec {
+
+  case class Checkout(item: String, creditCard: String)
+
+  case class AuthorizeCard(creditCard: String)
+
+  class CheckOutActor extends Actor {
+    private val paymentManager = context.actorOf(Props[PaymentManager])
+    private val fulfillmentManager = context.actorOf(Props[FulfillmentManager])
+
+    override def receive: Receive = ???
+
+    def awaitingCheckout: Receive = {
+      case Checkout(item, creditCard) =>
+        paymentManager ! AuthorizeCard(creditCard)
+    }
+  }
+
+  class PaymentManager extends Actor {
+    override def receive: Receive = ???
+  }
+
+  class FulfillmentManager extends Actor {
+    override def receive: Receive = ???
+  }
+
+}
