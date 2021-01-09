1bf55c4401f0a5d3b5d6117d12609299456c9f4b
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-09 22:38:23 +0330

    add InteceptionLogger

--- src/main/scala/part3testing/InterceptingLogSpec.scala
+++ src/main/scala/part3testing/InterceptingLogSpec.scala
@@ -3,7 +3,6 @@ package part3testing
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.testkit.{EventFilter, ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
-import part3testing.InterceptingLogSpec.Checkout
 
 class InterceptingLogSpec extends TestKit(ActorSystem("InterceptingLogSpec"))
   with ImplicitSender
@@ -14,6 +13,8 @@ class InterceptingLogSpec extends TestKit(ActorSystem("InterceptingLogSpec"))
     TestKit.shutdownActorSystem(system)
   }
 
+  import InterceptingLogSpec._
+
   val item = "Rock the JVM Akka course"
   val creditCard = "1234-1234-1234-1234"
 
@@ -21,12 +22,11 @@ class InterceptingLogSpec extends TestKit(ActorSystem("InterceptingLogSpec"))
     "correctly log the dispatch of an order" in {
       EventFilter.info(pattern = s"Order [0-9]+ for item $item has been dispatched!", occurrences = 1) intercept {
         // our test code
-        val checkoutRef = system.actorOf(Props[InterceptingLogSpec])
+        val checkoutRef = system.actorOf(Props[CheckOutActor])
         checkoutRef ! Checkout(item, creditCard)
       }
     }
   }
-
 }
 
 object InterceptingLogSpec {
