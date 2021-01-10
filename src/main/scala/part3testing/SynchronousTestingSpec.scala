aa2149664596c563e7350044d1897047bb3e4113
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-10 23:28:23 +0330

    counterHandler

--- src/main/scala/part3testing/SynchronousTestingSpec.scala
+++ src/main/scala/part3testing/SynchronousTestingSpec.scala
@@ -1,7 +1,6 @@
 package part3testing
 
 import akka.actor.Actor
-import akka.io.Inet.SO.ReuseAddress
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 class SynchronousTestingSpec extends WordSpecLike with BeforeAndAfterAll {
@@ -9,11 +8,15 @@ class SynchronousTestingSpec extends WordSpecLike with BeforeAndAfterAll {
 }
 
 object SynchronousTestingSpec {
+
   case object Inc
+
   case object Read
 
   class Counter extends Actor {
-    override def receive: Receive = {
+    override def receive: Receive = counterHendler()
+
+    def counterHendler(count: Int = 0): Receive = {
 
     }
   }
