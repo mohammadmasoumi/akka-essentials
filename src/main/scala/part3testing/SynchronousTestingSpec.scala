dc339f61cd60f2432be0b0e59c5c5f1901a2eba9
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-10 23:32:30 +0330

    add cases

--- src/main/scala/part3testing/SynchronousTestingSpec.scala
+++ src/main/scala/part3testing/SynchronousTestingSpec.scala
@@ -1,6 +1,6 @@
 package part3testing
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorLogging}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 class SynchronousTestingSpec extends WordSpecLike with BeforeAndAfterAll {
@@ -13,11 +13,18 @@ object SynchronousTestingSpec {
 
   case object Read
 
-  class Counter extends Actor {
-    override def receive: Receive = counterHendler()
+  class Counter extends Actor with ActorLogging {
+    override def receive: Receive = counterHandler()
 
-    def counterHendler(count: Int = 0): Receive = {
+    def counterHandler(count: Int = 0): Receive = {
+      case Inc =>
+        val newCount = count + 1
+        log.info(s"Incremented count is: $count")
+        context.become(counterHandler(newCount))
 
+      case Read =>
+        log.info(s"Reading count: $count")
+        sender() ! count
     }
   }
 
