3415bbfd61dc2c089deeae4350782b526148ea51
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-12 23:25:58 +0330

    add logging

--- src/main/scala/part4defaulttolerance/StartingStoppingActors.scala
+++ src/main/scala/part4defaulttolerance/StartingStoppingActors.scala
@@ -1,6 +1,6 @@
 package part4defaulttolerance
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{Actor, ActorLogging, ActorSystem}
 
 class StartingStoppingActors extends App {
 
@@ -11,7 +11,7 @@ class StartingStoppingActors extends App {
     override def receive: Receive = ???
   }
 
-  class Child extends Actor {
+  class Child extends Actor  with ActorLogging{
     override def receive: Receive = ???
   }
 
