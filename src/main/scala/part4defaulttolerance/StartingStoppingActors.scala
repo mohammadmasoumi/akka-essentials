7254fde6425a757f9bf495b2040d88ed8c00a9a0
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-12 23:32:54 +0330

    add withChildren

--- src/main/scala/part4defaulttolerance/StartingStoppingActors.scala
+++ src/main/scala/part4defaulttolerance/StartingStoppingActors.scala
@@ -1,20 +1,28 @@
 package part4defaulttolerance
 
-import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem}
 
 class StartingStoppingActors extends App {
 
   val system = ActorSystem("StoppingActorDemo")
 
 
-  class Parent extends Actor {
-    override def receive: Receive = ???
+  class Parent extends Actor with ActorLogging {
+    override def receive: Receive = withChildren(Map())
+
+    def withChildren(children: Map[String, ActorRef]): Receive = {
+
+    }
   }
 
   object Parent {
+
     case class StartChild(name: String)
+
     case class StopChild(name: String)
+
     case object Stop
+
   }
 
   class Child extends Actor with ActorLogging {
