d30e2bc594c8b48229f11e0a4681d6f90c14e37d
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-14 23:40:18 +0330

    override preStarting message

--- src/main/scala/part4faulttolerance/ActorLifeCycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifeCycle.scala
@@ -6,8 +6,10 @@ object ActorLifeCycle extends App {
 
   object StartChild
   class LifeCycleActor extends Actor with ActorLogging{
+
+    override def preStart(): Unit = log.info("I am starting.")
     override def receive: Receive = {
-      case StartChild => context.actorOf(Props[LifeCycleActor])
+      case StartChild => context.actorOf(Props[LifeCycleActor], "child")
 
     }
   }
