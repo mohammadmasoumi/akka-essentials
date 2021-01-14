4cbe37f03d6379f6618e978ff038f1acdd67ee8c
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-14 23:37:09 +0330

    add LifeCycleActor

--- src/main/scala/part4faulttolerance/ActorLifeCycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifeCycle.scala
@@ -1,6 +1,12 @@
 package part4faulttolerance
 
+import akka.actor.Actor
+
 object ActorLifeCycle extends App {
+
+  class LifeCycleActor extends Actor {
+    override def receive: Receive = ???
+  }
 
 
 }
