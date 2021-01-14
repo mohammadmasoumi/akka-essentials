9bfee49803140a4bf9bc481c84b3e104613670da
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-14 23:43:08 +0330

    add actorSystem and lifeCycle

--- src/main/scala/part4faulttolerance/ActorLifeCycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifeCycle.scala
@@ -1,11 +1,12 @@
 package part4faulttolerance
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 
 object ActorLifeCycle extends App {
 
   object StartChild
-  class LifeCycleActor extends Actor with ActorLogging{
+
+  class LifeCycleActor extends Actor with ActorLogging {
 
     override def preStart(): Unit = log.info("I am starting.")
 
@@ -13,9 +14,11 @@ object ActorLifeCycle extends App {
 
     override def receive: Receive = {
       case StartChild => context.actorOf(Props[LifeCycleActor], "child")
-
     }
   }
+
+  val system = ActorSystem("LifeCycleActor")
+  val parent = system.actorOf(Props[LifeCycleActor], "parent")
 
 
 }
