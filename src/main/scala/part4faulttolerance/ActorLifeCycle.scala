eedb8ca1f337b2b90c4c21896be03bce0e351056
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-14 23:37:37 +0330

    add ActorLogging

--- src/main/scala/part4faulttolerance/ActorLifeCycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifeCycle.scala
@@ -1,10 +1,10 @@
 package part4faulttolerance
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorLogging}
 
 object ActorLifeCycle extends App {
 
-  class LifeCycleActor extends Actor {
+  class LifeCycleActor extends Actor with ActorLogging{
     override def receive: Receive = ???
   }
 
