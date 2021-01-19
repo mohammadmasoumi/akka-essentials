5fc3fec67a8d51e2e3486b0909407a71e784eab6
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-19 22:28:56 +0330

    add treats

--- src/main/scala/part4faulttolerance/SuperVisionSpec.scala
+++ src/main/scala/part4faulttolerance/SuperVisionSpec.scala
@@ -1,9 +1,13 @@
 package part4faulttolerance
 
 import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
-class SuperVisionSpec extends TestKit(ActorSystem("SupervisionSpec")){
+class SuperVisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
 
 }
 
