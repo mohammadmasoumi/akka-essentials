7d27a16a08f26386227fc8199a417e192064904d
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-21 00:14:03 +0330

    add simpleActor

--- src/main/scala/part5infra/TimerSchedulers.scala
+++ src/main/scala/part5infra/TimerSchedulers.scala
@@ -1,5 +1,15 @@
 package part5infra
 
+import akka.actor.{Actor, ActorLogging, ActorSystem}
+
 object TimerSchedulers extends App {
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+
+  val system = ActorSystem("SchedulersTimerDemo")
 
 }
