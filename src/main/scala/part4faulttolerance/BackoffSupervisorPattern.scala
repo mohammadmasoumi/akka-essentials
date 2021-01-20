79d8326bda5353fe420a9bcebe5a21d06ecea574
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-20 23:12:16 +0330

    add handleIo

--- src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
+++ src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
@@ -2,11 +2,20 @@ package part4faulttolerance
 
 import akka.actor.{Actor, ActorLogging}
 
+import scala.io.Source
+
 object BackoffSupervisorPattern extends App {
 
   case object ReadFile
-  class FileBasePersistentActor extends Actor  with ActorLogging {
-    override def receive: Receive = ???
+
+  class FileBasePersistentActor extends Actor with ActorLogging {
+    override def receive: Receive = handleIO()
+
+    def handleIO(dataSource: Source = null): Receive = {
+
+    }
+
+
   }
 
 }
