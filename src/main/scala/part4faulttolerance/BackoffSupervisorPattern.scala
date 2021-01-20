f508056d0a4b1d5b2bd42f4b455ae73bba692999
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-20 23:10:55 +0330

    add ReadFile case object

--- src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
+++ src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging}
 
 object BackoffSupervisorPattern extends App {
 
+  case object ReadFile
   class FileBasePersistentActor extends Actor  with ActorLogging {
     override def receive: Receive = ???
   }
