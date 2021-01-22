eb116c8ddce5cd42f9002eec7f4a8eec11ee3274
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-22 23:05:32 +0330

    add comment

--- src/main/scala/part5infra/Routers.scala
+++ src/main/scala/part5infra/Routers.scala
@@ -4,8 +4,13 @@ import akka.actor.{Actor, ActorLogging}
 
 object Routers extends App {
 
+  /**
+   * #1 = manual router
+   */
   class Mater extends Actor with ActorLogging {
-    override def receive: Receive = ???
+    override def receive: Receive = {
+      ???
+    }
   }
 
   class Slave extends Actor with ActorLogging {
