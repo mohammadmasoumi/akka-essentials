f0564367fd0bf7a93b70f480181c39bb6d7ee945
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-22 23:08:03 +0330

    create slave

--- src/main/scala/part5infra/Routers.scala
+++ src/main/scala/part5infra/Routers.scala
@@ -1,6 +1,6 @@
 package part5infra
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, Props}
 
 object Routers extends App {
 
@@ -9,7 +9,7 @@ object Routers extends App {
    */
   class Mater extends Actor with ActorLogging {
     private val slaves = for (_ <- 1 to 5) yield {
-
+      val slave = context.actorOf(Props[Slave])
     }
 
     override def receive: Receive = {
