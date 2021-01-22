c8780d63687e7a9b60e9c9cc6e98c732e4802edd
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-22 23:02:04 +0330

    add Master class

--- src/main/scala/part5infra/Routers.scala
+++ src/main/scala/part5infra/Routers.scala
@@ -1,5 +1,11 @@
 package part5infra
 
+import akka.actor.{Actor, ActorLogging}
+
 object Routers extends App {
+
+  class Mater extends Actor with ActorLogging {
+    override def receive: Receive = ???
+  }
 
 }
