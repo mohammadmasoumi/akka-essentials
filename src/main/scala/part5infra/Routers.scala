d09415349e8292881fa18cec5020d4cd915fc8e7
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-22 23:06:44 +0330

    add slaves variable

--- src/main/scala/part5infra/Routers.scala
+++ src/main/scala/part5infra/Routers.scala
@@ -8,6 +8,10 @@ object Routers extends App {
    * #1 = manual router
    */
   class Mater extends Actor with ActorLogging {
+    private val slaves = for (_ <- 1 to 4) {
+
+    }
+
     override def receive: Receive = {
       ???
     }
