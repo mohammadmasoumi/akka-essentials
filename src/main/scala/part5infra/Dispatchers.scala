66dc485520507d0116fe2a4d7a27de70136eb39b
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-24 22:08:11 +0330

    add Counter actor

--- src/main/scala/part5infra/Dispatchers.scala
+++ src/main/scala/part5infra/Dispatchers.scala
@@ -1,6 +1,12 @@
 package part5infra
 
+import akka.actor.{Actor, ActorLogging}
+
 object Dispatchers extends App {
+
+  class Counter extends Actor with ActorLogging {
+    override def receive: Receive = ???
+  }
 
 }
 
