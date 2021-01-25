416d1f0e79d73aa0225cdca905c2061a6bee72f2
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-25 22:54:31 +0330

    add SimpleActor

--- src/main/scala/part5infra/Mailboxes.scala
+++ src/main/scala/part5infra/Mailboxes.scala
@@ -1,10 +1,14 @@
 package part5infra
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorSystem}
 
 object Mailboxes extends App {
 
   val system = ActorSystem("MailboxDemo")
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = ???
+  }
 
 
 }
