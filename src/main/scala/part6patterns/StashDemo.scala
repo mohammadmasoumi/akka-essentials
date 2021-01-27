b26e8218e532ec5919e220e6ce670c4be867e87d
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-27 23:31:51 +0330

    add comment

--- src/main/scala/part6patterns/StashDemo.scala
+++ src/main/scala/part6patterns/StashDemo.scala
@@ -5,7 +5,8 @@ object StashDemo extends App {
     /**
       ResourceActor
        - open => it can receivve read/write requests to the resource.
-             
+       - otherwise => it will postpone all read/write requests until the state is open.
+
     */
 
 }
