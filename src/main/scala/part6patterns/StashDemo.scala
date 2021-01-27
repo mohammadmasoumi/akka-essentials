d90065140b982a8bdf1b36e7abe7e93ac25a84fd
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-27 23:29:42 +0330

    add open state

--- src/main/scala/part6patterns/StashDemo.scala
+++ src/main/scala/part6patterns/StashDemo.scala
@@ -3,7 +3,9 @@ package part6patterns
 object StashDemo extends App {
 
     /**
-      ResourceActor      
+      ResourceActor
+       - open => it can receivve read/write requests to the resource.
+             
     */
 
 }
