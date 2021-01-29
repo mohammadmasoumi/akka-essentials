2448ff77e79c7d27091fab216030b463777bbb02
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-29 23:43:12 +0330

    add comment

--- src/main/scala/part6patterns/StashDemo.scala
+++ src/main/scala/part6patterns/StashDemo.scala
@@ -45,7 +45,7 @@ object StashDemo extends App {
         log.info("Opening resource")
         contest.become(open)
       case message =>
-        log.info(s"Stashing $message because i can't handle it in the closed state.")
+        log.info(s"Stashing $message because I can't handle it in the closed state.")
         stash()
     }
 
